3869edcccff3905fccf12e6012268918d4d6803e
Tim Williams <t.james.williams@gmail.com>
2017-03-11 10:44:12 -0800

    things

--- classes/jsonNavwalker.class.php
+++ classes/jsonNavwalker.class.php
@@ -12,26 +12,26 @@
 
 class jsonNavwalker extends Walker_Nav_Menu {
 
-  
+
   // Displays start of a level. E.g '<ul>'
   // @see Walker::start_lvl()
   function start_lvl(&$output, $depth=0, $args=array()) {
     $output .= "";
   }
- 
+
   // Displays end of a level. E.g '</ul>'
   // @see Walker::end_lvl()
   function end_lvl(&$output, $depth=0, $args=array()) {
     $output .= "";
   }
- 
+
   // Displays start of an element. E.g '<li> Item Name'
   // @see Walker::start_el()
-  function start_el(&$output, $item, $depth=0, $args=array()) {
+  function start_el(&$output, $item, $depth = 0, $args = [], $id = 0) {
     //$output = rtrim($output,',');
    // $output .= ",";
   }
- 
+
   // Displays end of an element. E.g '</li>'
   // @see Walker::end_el()
   function end_el(&$output, $item, $depth=0, $args=array()) {
@@ -61,14 +61,14 @@ class jsonNavwalker extends Walker_Nav_Menu {
 	public function display_element( $element, &$children_elements, $max_depth, $depth, $args, &$output ) {
     if ( ! $element )
                 return;
-    
-    
+
+
     $output .= json_encode($element);
     $id_field = $this->db_fields['id'];
-    
+
     if ( is_object( $args[0] ) )
         $args[0]->has_children = ! empty( $children_elements[ $element->$id_field ] );
-    
+
     parent::display_element( $element, $children_elements, $max_depth, $depth, $args, $output );
   }
 
